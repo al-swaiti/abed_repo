879a2a74f7aa501d9a6fb8106f2083e5584f3cfe
al-swaiti <abedalswaity7@gmail.com>
2022-12-24 22:40:32 +0300

    j

--- setup-git.sh
+++ setup-git.sh
@@ -86,7 +86,7 @@ case $CHOICE in
 			sudo git config --system core.editor nano
 			#git config --global credential.helper cache
 			#git config --global credential.helper 'cache --timeout=32000'
-      git remote set-url origin git@github.com:$githubdir/$project
+#       git remote set-url origin git@github.com:$githubdir/$project
       echo
       echo "Everything set"
       ;;
